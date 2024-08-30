f065572fac5f1b21bce16b9630d2afadce300691
ZapApriendo <isaiah.capers@outlook.com>
2024-08-29 23:59:58 -0400

    08.29.24

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,11 +1,13 @@
 export default function Nav({ links }) {
     return (
-      <nav className="navbar navbar-expand-lg bg-secondary">
+      <nav className="navbar navbar-expand-lg bg-secondary container-fluid fixed-top">
         <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse d-flex flex-row justify-content-between" id="navbarSupportedContent">
+         <div className ="text-white">
+            <h2>Isaiah</h2></div>
+            <div><ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-space-around">
               {links.map((link) => link)}
-            </ul>
+            </ul></div>
           </div>
         </div>
       </nav>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,14 @@
 import logo from '../assets/images/image1.png';
 export default function AboutPage(){
   return (
-    <div className="container pt-4">
-      <h2>About Me</h2>
-      <img src ={logo} alt="Photo"/>;
-      <p>
+    <div className="container-fluid pt-4">
+      <div><h2>About Me</h2></div>
+      <div>
+        <img src ={logo} alt="Photo" className="rounded-circle img-fluid"
+        style ={{width: '100px', height: '100px'}}/>
+      </div>
+      <div>
+        <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
         tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
         impedit suscipit sed magnam alias in, repellat expedita hic explicabo
@@ -12,7 +16,7 @@ export default function AboutPage(){
         adipisicing elit. Velit voluptate exercitationem quaerat pariatur
         mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
         nostrum temporibus ad omnis nam rerum eligendi.
-      </p>
+      </p></div>
     </div>
   );
 }
