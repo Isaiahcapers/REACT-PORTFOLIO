69be177dbcb1081091f39d91447c81760e5550ee
Isaiah Capers <isaiah.capers@outlook.com>
2024-09-02 20:52:33 -0400

    09.02.24

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,3 +1,33 @@
+import repoLinks from '../components/RepoLinks.jsx';
+const []
 export default function ResumePage () {
-    
+    return (
+        <div class="container pt-5">
+            <div>
+                <h2>Skills</h2>
+                <ul>
+                    <li>HTML,CSS,GIT</li>
+                    <li>JAVASCRIPT,TYPESCRIPT</li>
+                    <li>WEB API'S,SQL</li>
+                    <li>NODE JS</li>
+                    <li>REACT</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Front-End Projects</h2>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <p>My resume will go here</p>
+        </div>
+    );
+
+
+
+
+
 }
